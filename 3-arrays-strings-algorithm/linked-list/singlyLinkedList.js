645a2a048faca27b8506a286461d379a8ae4ce6c
Ashikur Rahman <ashikur35-540@diu.edu.bd>
2025-05-01 20:14:00 +0600

    update: Updated singly linked list code

--- 3-arrays-strings-algorithm/linked-list/singlyLinkedList.js
+++ 3-arrays-strings-algorithm/linked-list/singlyLinkedList.js
@@ -48,23 +48,21 @@ class SinglyLinkedList {
 
 
     // insert at index
-    insetAt(pos, val) {
+    insertAt(pos, val) {
 
         if (pos < 0 || pos > this.length) {
-            throw new Error("Invalid position!!")
+            throw new Error(`Invalid position = ${pos} for length = ${this.length}`)
         }
 
         // insert at first
         if (pos === 0) {
             this.prepend(val);
-            this.length++
             return true
         }
 
         // insert at last means after last element - if length is 4 then last element index is 3 and we need to append after that index which is 4
         if (pos === this.length) {
             this.append(val);
-            this.length++
             return true
         }
 
@@ -94,11 +92,132 @@ class SinglyLinkedList {
         return true;
     }
 
+    // remove the first occurrence
+    remove(data) {
+        // id empty list
+        if (!this.head) {
+            throw new Error("Empty List! Invalid Operation!");
+        }
+
+        // if that data is first node or head
+        if (this.head.data === data) {
+            // replace the current head with next of the current head 
+            this.head = this.head.next;
+            this.length--;
+
+            // if removed node was the tail
+            if (!current.next) this.tail = current;
+            return true
+        }
+
+        // loop the list to find the data and replace that with its next data
+        let current = this.head;
+        // last element's next will be null so we are looping until last
+        while (current.next) {
+            // as we already handled the first(head) we will look from the heads next
+            if (current.next.data === data) {
+                current.next = current.next.next
+                this.length--;
+                if (this.length === 0) this.tail = null
+                return true
+            }
+            // if not found then progress
+            current = current.next;
+
+        }
+        // codes comes here means we didn't find any so return false
+        return false
+
+    }
 
 
+    removeAt(index) {
+        // check valid
+        if (index < 0 || index >= this.length) {
+            throw new Error("Invalid operation!")
+        }
+        this.length--;
 
+        //  first index
+        if (index === 0) {
+            this.head = this.head.next
+            if (this.length === 1) this.tail = null; // assign tail for empty 
+            return true
+        }
+        // last index or in between in both cases we need to loop to track the previous node
+        else {
+            let current = this.head;
+            let prev = null
+            for (let i = 0; i < this.length; i++) {
+                prev = current
+                current = current.next;
+            }
+            // connect the prev to current's next node which opts out current
+            prev.next = current.next
+            if (!current.next) {
+                // if there is no element means removing last
+                this.tail = prev
+            }
 
+        }
+    }
 
+    // Get data at specific position
+	get(position) {
+		if (position < 0 || position >= this.size) {
+			return null;
+		}
+
+		let current = this.head;
+		let index = 0;
+
+		while (index < position) {
+			current = current.next;
+			index++;
+		}
+
+		return current.data;
+	}
+
+	// Check if list contains data
+	contains(data) {
+		let current = this.head;
+		while (current) {
+			if (current.data === data) {
+				return true;
+			}
+			current = current.next;
+		}
+		return false;
+	}
+
+	// Clear the list
+	clear() {
+		this.head = null;
+		this.tail = null;
+		this.size = 0;
+	}
+
+	// Get size of list
+	getSize() {
+		return this.size;
+	}
+
+	// Check if list is empty
+	isEmpty() {
+		return this.size === 0;
+	}
+
+	// Convert list to array
+	toArray() {
+		const array = [];
+		let current = this.head;
+		while (current) {
+			array.push(current.data);
+			current = current.next;
+		}
+		return array;
+	}
 
 }
 
@@ -119,7 +238,17 @@ console.log(newLinkList.head.data)
 console.log(newLinkList.tail.data)
 
 
-newLinkList.insetAt(2, 78)
+
+newLinkList.insertAt(2, 98) // valid
+newLinkList.insertAt(5, 78) // // invalid operation cz len is 4
+newLinkList.insertAt(6, 88) // invalid operation cz len is 4
+
 // list after insertion
-console.log(JSON.stringify(newLinkList, null, 2))
+console.log("After insertion ", JSON.stringify(newLinkList, null, 2))
+
+console.log(newLinkList.remove(78))
+console.log(newLinkList.remove(20))
+
+console.log("After remove~ ", JSON.stringify(newLinkList, null, 2))
+console.log(newLinkList.toArray())
 
